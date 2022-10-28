a692c3ff1aedeca45e85574616775977c704e5d9
unknown <md.ridgeway.work@gmail.com>
2022-10-27 21:00:39 -0500

    Initial Commit

---
+++ components/PostWidget.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link';
+
+import { getRecentPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(category, slug)
+        .then((result) => setRelatedPosts)
+    } else {
+      getRecentPosts()
+        .then((result) => setRelatedPosts)
+    }
+  }, [])
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostWidget
